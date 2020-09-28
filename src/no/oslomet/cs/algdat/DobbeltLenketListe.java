39cc37bbbd8c56005ffb2543af193c5be76c8710
asmar <asmar-nasar2000@hotmail.com>
2020-09-28 17:19:47 +0200

    Får feil på toString
    Får nullPointerExceptions på toStringmetoden. Prøver å fikse

--- src/no/oslomet/cs/algdat/DobbeltLenketListe.java
+++ src/no/oslomet/cs/algdat/DobbeltLenketListe.java
@@ -221,9 +221,7 @@ public class DobbeltLenketListe<T> implements Liste<T> {
         DobbeltLenketListe<String> l2 = new DobbeltLenketListe<>(s2);
         DobbeltLenketListe<String> l3 = new DobbeltLenketListe<>(s3);
         System.out.println(l1.toString() + " " + l2.toString()
-                + " " + l3.toString() + " " + l1.omvendtString() + " "
-                + l2.omvendtString() + " " + l3.omvendtString());
-
+                + " " + l3.toString());
     }
 
 } // class DobbeltLenketListe
